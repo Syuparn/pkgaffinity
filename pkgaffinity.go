521fc95b7e14dab0df33ab08ca2114aab555e7e3
syuparn <s.hello.spagetti@gmail.com>
2023-03-19 19:31:43 +0900

    use importspec

--- pkgaffinity.go
+++ pkgaffinity.go
@@ -24,14 +24,14 @@ func run(pass *analysis.Pass) (any, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+		(*ast.ImportSpec)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+		case *ast.ImportSpec:
+			if n.Path.Value == `"fmt"` {
+				pass.Reportf(n.Pos(), "import is \"fmt\"")
 			}
 		}
 	})
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,9 @@
 package a
 
+import (
+	"fmt" // want "import is \"fmt\""
+)
+
 func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "identifier is gopher"
-	print(gopher)  // want "identifier is gopher"
+	fmt.Println("hello")
 }
